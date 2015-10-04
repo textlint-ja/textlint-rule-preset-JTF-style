a7a8df00fb770adea1b312c846d9d893e8fc0d48
azu <azuciao@gmail.com>
2015-10-04 12:11:53 +0900

    refactor(rule): remove unused variable

--- src/2.2.2.算用数字と漢数字の使い分け.js
+++ src/2.2.2.算用数字と漢数字の使い分け.js
@@ -27,28 +27,83 @@ const numberMap = {
     "百": 100,
     "〇": 0
 };
-const kanNumberMap = {
-    "0": "〇",
-    "1": "一",
-    "2": "二",
-    "3": "三",
-    "4": "四",
-    "5": "五",
-    "6": "六",
-    "7": "七",
-    "8": "八",
-    "9": "九",
-    "０": "〇",
-    "１": "一",
-    "２": "二",
-    "３": "三",
-    "４": "四",
-    "５": "五",
-    "６": "六",
-    "７": "七",
-    "８": "八",
-    "９": "九"
-};
+// http://www.drk7.jp/MT/archives/001587.html
+function _num2ja(num, opt) {
+    var sign = {
+        '+': '',
+        '-': '−'
+    };
+    var zero = '零';
+    var point = '点';
+    var zero2nine = ['〇', '一', '二', '三', '四', '五', '六', '七', '八', '九'];
+    var ten2thou = ['', '十', '百', '千'];
+    var suffices = [
+        '', '万', '億', '兆', '京', '垓', '禾予', '穣', '溝', '澗', '正', '載,', '極', '恒河沙', '阿僧祇', '那由他', '不可思議',
+        '無量大数'
+    ];
+
+    num = num.replace(/,/g, '');
+    num.match(/([+-])?(\d+)(?:\.(\d+))?/i);
+    var sig = RegExp.$1;
+    var int = RegExp.$2;
+    var fract = RegExp.$3;
+    var seisuu = '';
+    var shins = [];
+
+    for (let i = int.length; i > 0; i -= 4) {
+        shins.push(int.substring(i, i - 4));
+    }
+    if (shins.length >= 18) {
+        return suffices[17];
+    }
+
+    var suffix = 0;
+    for (let i = 0; i < shins.length; i++) {
+        var shin = shins[i];
+        if (shin == '0000') {
+            suffix++;
+            continue;
+        }
+        var sens = '';
+        var keta = 0;
+        var digits = shin.split('').reverse();
+        for (var j = 0; j < digits.length; j++) {
+            var digit = digits[j];
+
+            if (opt['fixed4'] || opt['with_arabic']) {
+                if (opt['with_arabic']) {
+                    var flg = 0;
+                    // 余分な 0 を削除する
+                    if (digit == '0') {
+                        for (var k = j + 1; k < digits.length; k++) {
+                            flg += (digits[k] == '0') ? 0 : 1;
+                        }
+                        if (flg == 0) {
+                            digit = '';
+                        }
+                    }
+                    sens = digit + sens;
+                } else {
+                    sens = zero2nine[digit] + sens;
+                }
+            } else {
+                var suuji = (digit == 1 && !opt['p_one'] && keta > 0) ? '' : zero2nine[digit];
+                if (digit != 0) {
+                    sens = suuji + ten2thou[keta] + sens
+                }
+            }
+            keta++;
+        }
+        seisuu = sens + suffices[suffix++] + seisuu;
+    }
+    var result = (sign[sig] || '') + seisuu;
+    result = result || zero;
+    if (fract) {
+        result = result + point + fract;
+    }
+    return result;
+}
+
 // 2.2.2. 算用数字と漢数字の使い分け
 export default function (context) {
     let {Syntax, RuleError, report, getSource} = context;
@@ -70,84 +125,6 @@ export default function (context) {
                 report(node, new RuleError(`${text} => ${expected}`));
             };
 
-
-            function _num2ja(num, opt) {
-                var sign = {
-                    '+': '',
-                    '-': '−'
-                };
-                var zero = '零';
-                var point = '点';
-                var zero2nine = ['〇', '一', '二', '三', '四', '五', '六', '七', '八', '九'];
-                var ten2thou = ['', '十', '百', '千'];
-                var suffices = [
-                    '', '万', '億', '兆', '京', '垓', '禾予', '穣', '溝', '澗', '正', '載,', '極', '恒河沙', '阿僧祇', '那由他', '不可思議',
-                    '無量大数'
-                ];
-
-                num = num.replace(/,/g, '');
-                num.match(/([+-])?(\d+)(?:\.(\d+))?/i);
-                var sig = RegExp.$1;
-                var int = RegExp.$2;
-                var fract = RegExp.$3;
-                var seisuu = '';
-                var shousuu = '';
-                var shins = [];
-                var counter = 0;
-
-                for (let i = int.length; i > 0; i -= 4) {
-                    shins.push(int.substring(i, i - 4));
-                }
-                if (shins.length >= 18) {
-                    return suffices[17];
-                }
-
-                var suffix = 0;
-                for (let i = 0; i < shins.length; i++) {
-                    var shin = shins[i];
-                    if (shin == '0000') {
-                        suffix++;
-                        continue;
-                    }
-                    var sens = '';
-                    var keta = 0;
-                    var digits = shin.split('').reverse();
-                    for (var j = 0; j < digits.length; j++) {
-                        var digit = digits[j];
-
-                        if (opt['fixed4'] || opt['with_arabic']) {
-                            if (opt['with_arabic']) {
-                                var flg = 0;
-                                // 余分な 0 を削除する
-                                if (digit == '0') {
-                                    for (var k = j + 1; k < digits.length; k++) {
-                                        flg += (digits[k] == '0') ? 0 : 1;
-                                    }
-                                    if (flg == 0) {
-                                        digit = '';
-                                    }
-                                }
-                                sens = digit + sens;
-                            } else {
-                                sens = zero2nine[digit] + sens;
-                            }
-                        } else {
-                            var suuji = (digit == 1 && !opt['p_one'] && keta > 0) ? '' : zero2nine[digit];
-                            if (digit != 0) {
-                                sens = suuji + ten2thou[keta] + sens
-                            }
-                        }
-                        keta++;
-                    }
-                    seisuu = sens + suffices[suffix++] + seisuu;
-                }
-                var result = (sign[sig] || '') + seisuu;
-                result = result || zero;
-                if (fract) {
-                    result = result + point + fract;
-                }
-                return result;
-            }
 
             // 算用数字 -> 漢数字
             let toKanNumber = (text, pattern, match) => {
