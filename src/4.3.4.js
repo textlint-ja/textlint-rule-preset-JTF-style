3815c7bd5875c0d992f922f3b7a319aae5b4e822
azu <azuciao@gmail.com>
2015-10-09 20:30:11 +0900

    feat(rule): implement 4.3.4.二重かぎかっこ『』

---
+++ src/4.3.4.js
@@ -0,0 +1,13 @@
+// LICENSE : MIT
+"use strict";
+/*
+4.3.4.二重かぎかっこ『』
+文献の題を示す場合や、かぎかっこの中にさらにかぎかっこを入れる場合に使用します。
+ */
+import {checkPair} from "./util/pair-checker";
+export default function (context) {
+    return checkPair(context, {
+        left: "『",
+        right: "』"
+    });
+}
---
+++ test/4.3.4-test.js
@@ -0,0 +1,49 @@
+// LICENSE : MIT
+"use strict";
+import TextLintTester from "textlint-tester";
+import rule from "../src/4.3.4";
+var tester = new TextLintTester();
+tester.run("4.3.4.二重かぎかっこ『』", rule, {
+    valid: [
+        "彼は『『×××』を参照してください』と言った。",
+        `これは複数行の例
+
+彼は『『×××』を参照してください』と言った。
+        `,
+        "- 彼は『『×××』を参照してください』と言った。"
+    ],
+    invalid: [
+        {
+            text: "『対となるかっこがない文章です。",
+            errors: [
+                {
+                    message: "『の対となる』が見つかりません。『』",
+                    column: 0
+                }
+            ]
+        },
+        {
+            text: `『パラグラフをまたぐような
+
+文章』は認められない。
+            `,
+            errors: [
+                {
+                    message: "『の対となる』が見つかりません。『』",
+                    column: 0
+                }
+            ]
+        },
+        {
+            // ListItem -> Paragraphなので
+            text: "- 『これはプラグイン",
+            errors: [
+                {
+                    message: "『の対となる』が見つかりません。『』",
+                    column: 0
+                }
+            ]
+        }
+
+    ]
+});
