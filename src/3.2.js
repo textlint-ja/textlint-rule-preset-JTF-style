13736573f6ea6e73a42e2c158e8391e9e36272c5
azu <azuciao@gmail.com>
2015-10-09 22:07:51 +0900

    feat(rule): implement 3.2.カタカナ語間のスペースの有無

---
+++ src/3.2.js
@@ -0,0 +1,28 @@
+// LICENSE : MIT
+"use strict";
+/*
+3.2.カタカナ語間のスペースの有無
+中黒または半角スペースを用いてカタカナ語を区切ります。
+「2.1.7 カタカナ複合語」を参照してください。
+ */
+import {isUserWrittenNode} from "./util/node-util";
+export default function (context) {
+    let {Syntax, RuleError, report, getSource} = context;
+    return {
+        [Syntax.Str](node){
+            if (!isUserWrittenNode(node, context)) {
+                return;
+            }
+            let text = getSource(node);
+            let matchReg = /[ァ-ヶー]([^[ァ-ヶー])[ァ-ヶー]/;
+            let match = text.match(matchReg);
+            if (match) {
+                // カタカナの間を全角スペースでは区切らない
+                let word = match[1];
+                if (word === "　") {
+                    report(node, new RuleError("カタカナ語間は中黒（・）または半角スペースを用いてカタカナ語を区切ります", match.index + 1));
+                }
+            }
+        }
+    }
+}
---
+++ test/3.2-test.js
@@ -0,0 +1,25 @@
+// LICENSE : MIT
+"use strict";
+import TextLintTester from "textlint-tester";
+import rule from "../src/3.2";
+var tester = new TextLintTester();
+tester.run("3.2.カタカナ語間のスペースの有無", rule, {
+    valid: [
+        "カタカナ・カタカナ",
+        "カタカナ カタカナ",
+        "カタカナ、カタカナ",// 例外としてしょうがない気がする
+        "あいう　えお",
+        "インターフェース ブラウザ"
+    ],
+    invalid: [
+        {
+            text: "カタカナ　カタカナ",
+            errors: [
+                {
+                    message: "カタカナ語間は中黒（・）または半角スペースを用いてカタカナ語を区切ります",
+                    column: 4
+                }
+            ]
+        }
+    ]
+});
