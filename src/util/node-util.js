efc69685e7b8ac06a1bcf80680c45288ce13c4ce
azu <azuciao@gmail.com>
2015-10-19 09:02:03 +0900

    docs(util): add comment

--- src/util/node-util.js
+++ src/util/node-util.js
@@ -1,6 +1,13 @@
 // LICENSE : MIT
 "use strict";
 import {RuleHelper} from "textlint-rule-helper";
+/**
+ * ユーザーが書いたと推測されるNodeかどうかを判定する
+ * ユーザーが管理できないテキストは対象外としたいため。
+ * @param node
+ * @param context
+ * @returns {boolean}
+ */
 export function isUserWrittenNode(node, context) {
     let helper = new RuleHelper(context);
     let Syntax = context.Syntax;
