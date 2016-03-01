c5538fc64dc54193303b57c59aeb7b5da140842a
azu <azuciao@gmail.com>
2016-03-01 21:51:36 +0900

    chore(2.1.2): 正規表現をutilsへ移動

--- src/util/regexp.js
+++ src/util/regexp.js
@@ -1,5 +1,7 @@
 // LICENSE : MIT
 "use strict";
-export const japaneseRegExp = /(?:[\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F][\uDC00-\uDFFF]|[ぁ-んァ-ヶ])/;
+export const japaneseRegExp = /(?:[々〇〻\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F][\uDC00-\uDFFF]|[ぁ-んァ-ヶ])/;
+// http://tama-san.com/kanji-regex/
+export const kanjiRegExp = /(?:[々〇〻\u3400-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F][\uDC00-\uDFFF])/;
 export const hiraganaRegExp = /[ぁ-ん]/;
 export const karakanaRegExp = /[ァ-ヶ]/;
