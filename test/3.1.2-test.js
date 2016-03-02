2ed576e78ade0196bc3589de9f51cc205d5fc7a4
azu <azuciao@gmail.com>
2016-03-02 22:17:19 +0900

    feat(3.1.2): fixer support

--- test/3.1.2-test.js
+++ test/3.1.2-test.js
@@ -6,14 +6,29 @@ var tester = new TextLintTester();
 tester.run("3.1.2. 全角文字どうし", rule, {
     valid: [
         "これは正解",
+        "This is 大丈夫",
         "This is a pen.",
-        "ユーザー インターフェース"//例外
+        "ユーザー インターフェース"//カタカナは例外
     ],
     invalid: [
         {
             text: "これは ダメ",
+            output: "これはダメ",
             errors: [
-                {message: "原則として、全角文字どうしの間にスペースを入れません。"}
+                {
+                    message: "原則として、全角文字どうしの間にスペースを入れません。",
+                    column: 4
+                }
+            ]
+        },
+        {
+            text: "これは どういうこと?",
+            output: "これはどういうこと?",
+            errors: [
+                {
+                    message: "原則として、全角文字どうしの間にスペースを入れません。",
+                    column: 4
+                }
             ]
         }
     ]
