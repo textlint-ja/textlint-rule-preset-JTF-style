faa5ec27e9e6a5c736550ab2e0e5d0f9f4f15564
azu <azuciao@gmail.com>
2016-05-02 00:32:35 +0900

    feat(monorepo): add 1.1.2 caption

---
+++ packages/textlint-rule-jtf-style-captions/src/1.1.2.js
@@ -0,0 +1,44 @@
+// LICENSE : MIT
+"use strict";
+/*
+1.1.2.見出し
+
+本文が敬体であっても、見出しには常体や体言止めを使います。
+一般向けのマニュアルでは、「~には」や「~とは」などの助詞で止める文形も使います。
+見出しの文末には、句点(。)を付けません。
+
+# Header
+
+のみをサポート
+
+Header
+=======
+
+は無視する
+ */
+import {isUserWrittenNode} from "./util/node-util";
+function mixer(context) {
+    let {Syntax, RuleError, report, getSource, fixer} = context;
+    return {
+        [Syntax.Header](node){
+            if (!isUserWrittenNode(node, context)) {
+                return;
+            }
+            let text = getSource(node);
+            // Headerの末尾には。をつけない
+            let matchReg = /。(\s*?)$/;
+            let index = text.search(matchReg);
+            if (index !== -1) {
+                report(node, new RuleError("見出しの文末には、句点(。)を付けません。", {
+                    index: index,
+                    fix: fixer.removeRange([index, index + 1])
+                }));
+            }
+            // TODO: いずれの場合も、すべての見出しを通して複数の文体をできるだけ混在させないことが重要です。
+        }
+    }
+}
+export default {
+    linter: mixer,
+    fixer: mixer
+}
---
+++ packages/textlint-rule-jtf-style-captions/test/1.1.2-test.js
@@ -0,0 +1,47 @@
+// LICENSE : MIT
+"use strict";
+import TextLintTester from "textlint-tester";
+import rule from "../src/1.1.2";
+var tester = new TextLintTester();
+tester.run("1.1.2.見出し", rule, {
+    valid: [
+        "# 見出しである\n\n本文はですます調です。",
+        "## 見出しです",
+        "### モーニング娘。について"// 。が最後ではない
+    ],
+    invalid: [
+        {
+            text: "# 見出し。",
+            output: "# 見出し",
+            errors: [
+                {
+                    message: "見出しの文末には、句点(。)を付けません。",
+                    line: 1,
+                    column: 6
+                }
+            ]
+        },
+        {
+            text: "### 見出し。",
+            output: "### 見出し",
+            errors: [
+                {
+                    message: "見出しの文末には、句点(。)を付けません。",
+                    line: 1,
+                    column: 8
+                }
+            ]
+        },
+        {
+            text: "### 見出し。\n test",
+            output: "### 見出し\n test",
+            errors: [
+                {
+                    message: "見出しの文末には、句点(。)を付けません。",
+                    line: 1,
+                    column: 8
+                }
+            ]
+        }
+    ]
+});
