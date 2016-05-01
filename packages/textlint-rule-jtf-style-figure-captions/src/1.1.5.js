7d1e3422a65000b10ac5a99dc3e2d31696c3df16
azu <azuciao@gmail.com>
2016-05-02 00:36:26 +0900

    feat(monorepo): add 1.1.5.図表のキャプション

---
+++ packages/textlint-rule-jtf-style-figure-captions/src/1.1.5.js
@@ -0,0 +1,84 @@
+// LICENSE : MIT
+"use strict";
+/*
+1.1.5.図表のキャプション
+文章の内容に応じて、敬体、常体、体言止めを使います。
+いずれの場合も、複数の文体をできるだけ混在させないことが重要です。
+通常、文末に句点(。)を付けませんが、複数の文章になる場合は句点を使用します。
+
+キャプション間で文体が混ざっていないことを確認する。
+ */
+import {analyzeDesumasu, analyzeDearu} from "analyze-desumasu-dearu";
+export default function (context) {
+    let {Syntax, RuleError, report, getSource} = context;
+    let desumasuList = [];
+    let dearuList = [];
+
+    function resetState() {
+        desumasuList = [];
+        dearuList = [];
+    }
+
+    const imagePaddingLet = 2;// ![ の分paddingを付ける
+    function reportResult(list, {desumasu,dearu}) {
+        list.forEach(({node, matches}) => {
+            matches.forEach(match => {
+                let message;
+                if (desumasu) {
+                    message = `図表のキャプションを敬体(ですます調)に統一して下さい。\n図表のキャプション内で敬体、常体を混在させないことが重要です。\n"${match.value}"が常体(である調)です。`
+                } else if (dearu) {
+                    message = `図表のキャプションを常体(である調)に統一して下さい。\n図表のキャプション内で敬体、常体を混在させないことが重要です。\n"${match.value}"が敬体(ですます調)です。`
+                }
+                report(node, new RuleError(message, {
+                    line: match.lineNumber - 1,
+                    column: match.columnIndex + imagePaddingLet
+                }));
+            });
+        });
+    }
+
+    return {
+        [Syntax.Document]: resetState,
+        [Syntax.Image](node){
+            let text = node.alt;
+            // alt がない場合は無視する
+            if (text === undefined || text === null) {
+                return;
+            }
+            let retDesumasu = analyzeDesumasu(text);
+            if (retDesumasu.length > 0) {
+                desumasuList.push({
+                    node: node,
+                    matches: retDesumasu
+                });
+            }
+            let retDearu = analyzeDearu(text);
+            if (retDearu.length > 0) {
+                dearuList.push({
+                    node: node,
+                    matches: retDearu
+                });
+            }
+        },
+        [`${Syntax.Document}:exit`](){
+            let desumasuCount = desumasuList.reduce((count, {matches}) => count + matches.length, 0);
+            let dearuCount = dearuList.reduce((count, {matches}) => count + matches.length, 0);
+            if (desumasuCount === 0 || dearuCount === 0) {
+                return;
+            }
+            if (desumasuCount > dearuCount) {
+                reportResult(dearuList, {
+                    desumasu: true
+                });
+            } else if (desumasuCount < dearuCount) {
+                reportResult(desumasuList, {
+                    dearu: true
+                });
+            } else {
+                reportResult(dearuList, {
+                    desumasu: true
+                });
+            }
+        }
+    }
+}
---
+++ packages/textlint-rule-jtf-style-figure-captions/test/1.1.5-test.js
@@ -0,0 +1,45 @@
+// LICENSE : MIT
+"use strict";
+import TextLintTester from "textlint-tester";
+import rule from "../src/1.1.5";
+var tester = new TextLintTester();
+tester.run("1.1.5.図表のキャプション", rule, {
+    valid: [
+        "![これは図です。](http://example.com/img) と ![これは図です。](http://example.com/img)",
+        "![これは図である。](http://example.com/img) と ![これは図である。](http://example.com/img)",
+        "![](http://example.com/img)"
+    ],
+    invalid: [
+        {
+            text: "![これは図です。](http://example.com/img) と ![これは図です。](http://example.com/img)  と ![これは図である。](http://example.com/img).",
+            errors: [
+                {
+                    message: '図表のキャプションを敬体(ですます調)に統一して下さい。\n図表のキャプション内で敬体、常体を混在させないことが重要です。\n"である。"が常体(である調)です。',
+                    line: 1,
+                    column: 82
+                }
+            ]
+        },
+        {
+            text: "![これは図です。](http://example.com/img) と ![これは図である。](http://example.com/img)  と ![これは図である。](http://example.com/img).",
+            errors: [
+                {
+                    message: '図表のキャプションを常体(である調)に統一して下さい。\n図表のキャプション内で敬体、常体を混在させないことが重要です。\n"です。"が敬体(ですます調)です。',
+                    line: 1,
+                    column: 7 // imgのposition
+                }
+            ]
+        },
+        // 同数ならであるを注意
+        {
+            text: "![これは図です。](http://example.com/img) と ![これは図である。](http://example.com/img).",
+            errors: [
+                {
+                    message: '図表のキャプションを敬体(ですます調)に統一して下さい。\n図表のキャプション内で敬体、常体を混在させないことが重要です。\n"である。"が常体(である調)です。',
+                    line: 1,
+                    column: 44
+                }
+            ]
+        }
+    ]
+});
